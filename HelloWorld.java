0366073731c8ec7ca49732896e4bad8992b318ae
v.iruthuvarnarajan <sec21it155@sairamtap.edu.in>
2026-01-02 15:39:55 +0530

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello World");
+
+    }
+}
