e3435e227a3b99c350e6aeacc8d849a3d1e7dc63
v.iruthuvarnarajan <sec21it155@sairamtap.edu.in>
2026-01-06 14:24:17 +0530

    Encapsulation

---
+++ HashMap1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HashMap1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the String: ");
+        String s=sc.next();
+        HashMap<Character,Integer> freq=new HashMap<>();
+        for(char c:s.toCharArray()){
+            freq.put(c,freq.getOrDefault(c,0)+1);
+        }
+        System.out.println(freq);
+    }
+}
