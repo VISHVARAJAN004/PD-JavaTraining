f1f35e175317ec05afe8a4473fccfa6dd4f5dc2d
v.iruthuvarnarajan <sec21it155@sairamtap.edu.in>
2026-01-06 14:27:51 +0530

    HashMap

--- HashMapSample.java
+++ HashMapSample.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class HashMap1{
+public class HashMapSample{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the String: ");
